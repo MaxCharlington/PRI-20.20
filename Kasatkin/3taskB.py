96a6be75d0b1703bee4faca90fbd92c84a1ff7c4
pegasik666 <95439483+pegasik666@users.noreply.github.com>
2022-01-07 14:15:45 +0300

    Delete Kasatkin directory
    я запутался, создам новую ветку и залью заново

--- Kasatkin/3taskB.py
+++
@@ -1,8 +0,0 @@
-def stringCounter(s):
-    return len(s) - s.count(".") - s.count("-")
-
-def main():
-    print('введите число:')
-    print('колличество цифр - ', stringCounter(input()))
-
-main()
